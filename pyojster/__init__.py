ca66b09015fce7f10fedcca3edeeda77bfd7db2e
Yakov Borevich <jjay@ostrovok.ru>
2013-01-28 18:16:53 +0400

    Fix ojster cmdline call

--- pyojster/__init__.py
+++ pyojster/__init__.py
@@ -4,7 +4,7 @@ from subprocess import check_call
 def compile(source, target):
     cdir = os.path.abspath(os.path.dirname(__file__))
     ojster = os.path.join(cdir, 'ojster/bin/ojster')
-    cmd = [ojster, source, target]
+    cmd = [ojster, source, target, '--ext', '.ojst.js', '--goog']
     print "Running ojster: ", ' '.join(cmd)
     check_call(cmd)
     print "Done"
